b3c924721e27070fa747fa4729eec000330d4a4c
haliphax <haliphax@gmail.com>
2021-09-28 22:16:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const RESPONSES = [
+	'<3',
+	'BloodTrail',
+	'GlitchNRG',
+	'HolidaySanta',
+	'KevinTurtle',
+	'OhMyDog',
+	'PogChamp',
+	'Poooound',
+	'RyuChamp',
+	'SabaPing',
+	'SeemsGood',
+	'SMOrc',
+	'TBAngel',
+	'TriHard',
+	'VoteYea',
+];
+
+const qs = Object.fromEntries( window.location.href.split('?')[1].split('&').map(v => v.split('=')));
+const twitch = new tmi.Client({
+	channels: [qs.channel],
+	identity: {
+		username: qs.username,
+		password: `oauth:${qs.oauth}`,
+	},
+});
+
+twitch.on('message', (channel, tags, message, self) => {
+	if (self) return;
+
+	if (tags.username == 'streamcaptainbot'
+		&& message.indexOf(' just placed a') >= 0)
+	{
+		const choice = Math.floor(Math.random() * RESPONSES.length);
+		const who = message.substring(0, message.indexOf(' '));
+		const resp = RESPONSES[choice] + (qs.includeUsername ? ` ${who}` : '');
+
+		twitch.say(channel, resp);
+	}
+});
+
+twitch.connect();
